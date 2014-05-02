8b9d0c99047a78a16770e6dd46987f281e623887
William Dahl <william.a.dahl@gmail.com>
2014-05-02 15:45:51 +1000

    Initial commit

---
+++ app/src/main/java/com/williamdahl/databaseexample/app/Comment.java
@@ -0,0 +1,31 @@
+package com.williamdahl.databaseexample.app;
+
+/**
+ * Created by Will on 2/05/2014.
+ */
+public class Comment {
+    private long id;
+    private String comment;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    // Will be used by the ArrayAdapter in the ListView
+    @Override
+    public String toString() {
+        return comment;
+    }
+}
